9e5a74d36ba97013f3c62f293da16266e4a962a4
Tomasz Domański <tdomanski@sii.pl>
2020-02-13 14:47:39 +0100

    Warn about configuration change

--- src/extension.ts
+++ src/extension.ts
@@ -5,6 +5,8 @@ import openActionsForIssue from "./commands/open-actions-for-issue";
 import openActionsForIssueUnderCursor from "./commands/open-actions-for-issue-under-cursor";
 import listOpenIssuesAssignedToMe from "./commands/list-open-issues-assigned-to-me";
 import newIssue from "./commands/new-issue";
+import { RedmineConfig } from "./definitions/redmine-config";
+import { ActionProperties } from "./commands/action-properties";
 
 export function activate(context: vscode.ExtensionContext) {
   const bucket = {
@@ -16,7 +18,7 @@ export function activate(context: vscode.ExtensionContext) {
     return vscode.window
       .showWorkspaceFolderPick()
       .then(v => {
-        const config = vscode.workspace.getConfiguration("redmine", v.uri);
+        const config = vscode.workspace.getConfiguration("redmine", v.uri) as unknown as RedmineConfig;
         const redmineServer = new RedmineServer({
           address: config.url,
           key: config.apiKey
@@ -26,8 +28,8 @@ export function activate(context: vscode.ExtensionContext) {
           bucket.servers.find(s => s.compare(redmineServer)) || redmineServer;
 
         return {
-            server,
-            config
+          server,
+          config
         };
       })
       .then(
@@ -39,7 +41,44 @@ export function activate(context: vscode.ExtensionContext) {
       );
   };
 
-  const registerCommand = (name, action) => {
+  const currentConfig = vscode.workspace.getConfiguration("redmine");
+
+  if (currentConfig.has("serverUrl")) {
+    const panel = vscode.window.createWebviewPanel(
+      "redmineConfigurationUpdate",
+      "vscode-redmine: New configuration arrived!",
+      vscode.ViewColumn.One,
+      {}
+    );
+
+    panel.webview.html = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>vscode-redmine: New configuration arrived!</title>
+        <style>html, body { font-size: 16px; } p, li { line-height: 1.5; }</style>
+    </head>
+    <body>
+        <h1>vscode-redmine: New configuration arrived!</h1>
+        <p>Thanks for using <code>vscode-redmine</code>! From version 1.0.0, an old configuration schema has changed. We've detected, that you still use old format, so please update it to the new one.</p>
+        <p>
+            Following changed:
+            <ul>
+                <li><code>redmine.serverUrl</code>, <code>redmine.serverPort</code> and <code>redmine.serverIsSsl</code> became single setting: <code>redmine.url</code>.<br />
+                If you had <code>serverUrl = 'example.com/test'</code>, <code>serverPort = 8080</code> and <code>serverIsSsl = true</code>, then new <code>url</code> will be <code>https://example.com:8080/test</code>.</li>
+                <li><code>redmine.projectName</code> became <code>redmine.identifier</code>. Behavior remains the same</li>
+                <li><code>redmine.authorization</code> is deprecated. If you want to add <code>Authorization</code> header to every request sent to redmine, provide <code>redmine.additionalHeaders</code>, eg.:
+                    <pre>{"redmine.additionalHeaders": {"Authorization": "Basic 123qwe"}}</pre>
+                </li>
+            </ul>
+        </p>
+    </body>
+    </html>`;
+  }
+
+  const registerCommand = (name: string, action: (props: ActionProperties) => any) => {
     context.subscriptions.push(
       vscode.commands.registerCommand(`redmine.${name}`, () => {
         parseConfiguration().then(action);
@@ -49,7 +88,10 @@ export function activate(context: vscode.ExtensionContext) {
 
   registerCommand("listOpenIssuesAssignedToMe", listOpenIssuesAssignedToMe);
   registerCommand("openActionsForIssue", openActionsForIssue);
-  registerCommand("openActionsForIssueUnderCursor", openActionsForIssueUnderCursor);
+  registerCommand(
+    "openActionsForIssueUnderCursor",
+    openActionsForIssueUnderCursor
+  );
   registerCommand("newIssue", newIssue);
 }
 
